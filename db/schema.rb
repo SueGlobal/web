addd859aa78f285bae694beed31dfdae055eb9d5
Sergio Arbeo <serabe@gmail.com>
2012-09-15 01:08:33 +0200

    Updates db/schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120912123608) do
+ActiveRecord::Schema.define(:version => 20120914224812) do
 
   create_table "users", :force => true do |t|
     t.string   "username",         :null => false
