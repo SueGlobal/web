d133199208ec6584a7db6ff0075dac9d99a36111
Sergio Arbeo <serabe@gmail.com>
2012-09-15 23:36:06 +0200

    Some changes in db weren't picked up.

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(:version => 20120914224812) do
 
   create_table "users", :force => true do |t|
-    t.string   "username",         :null => false
     t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@
 
 FactoryGirl.define do
   factory :user do
-    sequence(:username) { |n| "user#{n}" }
     sequence(:email) { |n| "user#{n}@example.com" }
     password "123456"
   end
