5ef8a2b5bf434fe5373d932a17ef0ef471460256
Sergio Arbeo <serabe@gmail.com>
2012-09-17 10:40:27 +0200

    Adds routing spec for UserSessions.

---
+++ spec/routing/user_sessions_routing_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+describe UsersController do
+  describe "routing" do
+
+    it "routes to #new" do
+      get("/login").should route_to("user_sessions#new")
+    end
+
+    it "routes to #new" do
+      post("/user_sessions").should route_to("user_sessions#create")
+    end
+
+    it "routes to #destroy" do
+      get("/logout").should route_to("user_sessions#destroy")
+    end
+  end
+end
