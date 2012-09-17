9fa816d6ea358be01bce3880e0897db79437f628
Sergio Arbeo <serabe@gmail.com>
2012-09-17 13:15:13 +0200

    Adds logout integration test.

---
+++ spec/requests/logout_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Logout" do
+  describe "GET /logout" do
+    let!(:user) { create :user }
+
+    before(:each) do
+      login_with user.email, user.password
+      visit logout_path
+    end
+
+    it "redirects to root" do
+      current_path.should == root_path
+    end
+
+    it "shows a notice" do
+      page.should have_css("p#notice")
+    end
+
+    it "shows login option" do
+      page.should have_content("Login")
+    end
+  end
+end
