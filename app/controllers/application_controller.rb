b472325a6613c306e84adcec3a52d941caa870ef
Sergio Arbeo <serabe@gmail.com>
2012-09-19 13:24:47 +0200

    Support for old cancan.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from CanCan::AccessDenied do |exception|
+    Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
+    redirect_to root_url, notice: 'You are not authorized.'
+  end
 end
