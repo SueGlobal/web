3b8e0cdce49ef69ba182d96d591735cda23fbb5b
Sergio Arbeo <serabe@gmail.com>
2012-09-24 10:40:41 +0200

    Add before_filter for setting the locale.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
   rescue_from CanCan::AccessDenied do |exception|
     Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     Rails.logger.info "Someone tried to access without authentication."
     redirect_to root_url, notice: 'You need to log in to access that page.'
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.locale
   end
 end
