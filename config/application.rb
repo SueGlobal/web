fabe0947da947141ee6f39120d336e2658ae1e9f
Sergio Arbeo <serabe@gmail.com>
2012-09-24 14:47:43 +0200

    Added some basic translations.

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,9 @@ module SueGlobal
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**',  '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
+    config.i18n.available_locales = [:es, :en]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
