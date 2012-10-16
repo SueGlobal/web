ed1873b0354c8d6540f2eb3c43a652406bfee709
Sergio Arbeo <serabe@gmail.com>
2012-10-16 19:55:29 +0200

    Minor changes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -30,7 +30,7 @@ if env == "production"
   working_directory current_path
 
   # feel free to point this anywhere accessible on the filesystem
-  user 'sue', 'users'
+  user 'sue', 'sue'
 
   stderr_path "#{shared_path}/log/unicorn.stderr.log"
   stdout_path "#{shared_path}/log/unicorn.stdout.log"
