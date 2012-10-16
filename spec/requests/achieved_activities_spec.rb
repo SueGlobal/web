cbfc1b9e5e11f1d0535815474c80610a60aa4ba2
Sergio Arbeo <serabe@gmail.com>
2012-10-16 15:06:05 +0200

    Changes checkboxes order.

--- spec/requests/achieved_activities_spec.rb
+++ spec/requests/achieved_activities_spec.rb
@@ -4,4 +4,15 @@ require 'spec_helper'
 describe "AchievedActivities" do
   describe "GET /achieved_activities" do
   end
+
+  describe "add new activity" do
+    it "something" do
+      university = create :university
+      password = '123456'
+      user = create :user, :active, password: password, university: university
+
+      login_with user.email, password
+      visit new_university_achieved_activity_path(university)
+    end
+  end
 end
