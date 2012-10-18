7d4d5e8b724d1fe606196c04d9d3b778e0f183d0
Sergio Arbeo <serabe@gmail.com>
2012-10-18 13:04:43 +0200

    Add validations to basic question.

--- app/models/basic_question.rb
+++ app/models/basic_question.rb
@@ -2,4 +2,13 @@ class BasicQuestion < ActiveRecord::Base
   attr_accessible :author, :population, :title, :year
 
   belongs_to :studiable, polymorphic: true
+
+  validates :title,
+    presence: true
+
+  validates :author,
+    presence: true
+
+  validates :year,
+    numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
