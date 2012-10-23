1fc0c5d4f3896e28800bb3d662689f0975d0d50a
Sergio Arbeo <serabe@gmail.com>
2012-10-23 14:40:39 +0200

    Check if user can create universities before showing the link.

--- spec/views/universities/index.html.erb_spec.rb
+++ spec/views/universities/index.html.erb_spec.rb
@@ -13,4 +13,20 @@ describe "universities/index" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
   end
+
+  it "renders a new link if user can create a university" do
+    view.stub(:can?).with(any_args()).and_return true
+    view.stub(:can?).with(:new, University).and_return true
+    render
+
+    expect(rendered).to include(new_university_path)
+  end
+
+  it "renders no new link if user cannot create a university" do
+    view.stub(:can?).with(any_args()).and_return true
+    view.stub(:can?).with(:new, University).and_return false
+    render
+
+    expect(rendered).not_to include(new_university_path)
+  end
 end
