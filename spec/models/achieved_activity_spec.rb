c2e7fc1dde50fa7898e72ff221e66170a07390b7
Sergio Arbeo <serabe@gmail.com>
2012-10-24 14:54:58 +0200

    Some minor problems with achieved activities resolved.

--- spec/models/achieved_activity_spec.rb
+++ spec/models/achieved_activity_spec.rb
@@ -2,16 +2,21 @@
 require 'spec_helper'
 
 describe AchievedActivity do
-  subject { create :achieved_activity }
 
-  AchievedActivity::ACTIVITIES.each do |activity|
-    it { should allow_mass_assignment_of(activity) }
+  describe "fields accessors" do
+
+    subject { create :achieved_activity }
+
+    AchievedActivity::ACTIVITIES.each do |activity|
+      it { should allow_mass_assignment_of(activity) }
+    end
+
+    AchievedActivity::SOURCES.each do |source|
+      it { should allow_mass_assignment_of(source) }
+    end
+
+    it { should allow_mass_assignment_of(:year) }
+    #   Done in before filter
+    #  it { should validate_numericality_of(:year).only_integer }
   end
-
-  AchievedActivity::SOURCES.each do |source|
-    it { should allow_mass_assignment_of(source) }
-  end
-
-  it { should allow_mass_assignment_of(:year) }
-  it { should validate_numericality_of(:year).only_integer }
 end
