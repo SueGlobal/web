45b7c98a0a6e1bc659e8de74c959be983730571b
Sergio Arbeo <serabe@gmail.com>
2012-11-05 11:47:23 +0100

    Adds options to periodicity.

--- app/models/periodicity.rb
+++ app/models/periodicity.rb
@@ -3,12 +3,16 @@ class Periodicity < ActiveRecord::Base
   belongs_to :periodic, polymorphic: true
   attr_accessible :other_description, :periodicity_type
 
-  TYPES = [:anual, :six_monthly, :four_monthly, :no_periodicity, :other]
+  TYPES = [:three_years, :two_years, :anual, :six_monthly, :four_monthly, :three_monthly, :no_periodicity, :other]
 
   TYPES.each do |t|
     define_method :"#{t}?" do
       self.periodicity_type.to_sym == t
     end
+  end
+
+  def periodicity_type
+    self[:periodicity_type] || "anual"
   end
 
   class << self
