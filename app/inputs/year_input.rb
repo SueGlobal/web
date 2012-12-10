6a47c6afce3605a6ffed33db3369a5aa11b9f638
Sergio Arbeo <serabe@gmail.com>
2012-12-10 12:34:01 +0100

    Changes course to year in achieved activity related views.

---
+++ app/inputs/year_input.rb
@@ -0,0 +1,30 @@
+# -*- encoding : utf-8 -*-
+class YearInput < SimpleForm::Inputs::CollectionSelectInput
+
+  def detect_collection_methods
+    label, value = options.delete(:label_method), options.delete(:value_method)
+
+    label ||= :to_s
+    value ||= :to_i
+
+    [label, value]
+  end
+
+  def collection
+    @collection ||= begin
+      collection = options.delete(:collection) || build_year_arrays
+      collection.respond_to?(:call) ? collection.call : collection.to_a
+    end
+  end
+
+  protected
+  def build_year_arrays
+    opts = {start_year: Time.now.year, end_year: APP_CONFIG[:start_year]}.merge options
+
+    opts[:start_year].step(opts[:end_year], (opts[:start_year] > opts[:end_year]) ? -1 : 1)
+  end
+
+  def skip_include_blank?
+    true
+  end
+end
