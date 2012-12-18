d7c0b41cc542b54ff8a8efe5192d53a0f681aa2d
Sergio Arbeo <serabe@gmail.com>
2012-12-18 10:07:45 +0100

    Solves a problem with pages.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ SueGlobal::Application.routes.draw do
   get "summary/(:year)", to: 'summary#index', as: :summary
 
   # HighVoltage
-  get "/pages/*id" => "pages#show", as: :page, format: false
+  get "/pages/:id" => "pages#show", as: :page, format: false
   get "accept_change_request/:token", to: "university_change_request#accept", as: "accept_change_request"
 
   resources :announcements
